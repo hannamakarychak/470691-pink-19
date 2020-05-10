c7c1581cfe6617acd4f3f0eb51cca5a001c69b6d
hannamakarychak <annamakarichak3@gmail.com>
2020-05-10 16:44:08 +0200

    fix map section

--- source/js/table.js
+++ source/js/table.js
@@ -1,14 +1,15 @@
 function actualResizeHandler() {
   const offersCells = document.querySelectorAll(
-    ".offers__name:not(.offers__name--blank)"
+    ".offers__name--js:not(.offers__name--blank-js)"
   );
   const offersCellsArray = Array.prototype.slice.call(offersCells);
 
   const width = window.innerWidth;
 
-  offersCellsArray.forEach((cell) =>
-    cell.setAttribute("colspan", width >= 660 ? 1 : 2)
-  );
+  for (let index = 0; index < offersCellsArray.length; index++) {
+    const cell = offersCellsArray[index];
+    cell.setAttribute("colspan", width >= 660 ? 1 : 2);
+  }
 }
 
 var resizeTimeout;
